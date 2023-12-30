d00be036abdca0dfbc14c84e2c2c3608d8526197
unilock <unilock@fennet.rentals>
2023-12-30 11:12:50 -0500

    simplify mixin
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/ae2mixin/mixin/client/MEStorageScreenMixin.java
+++ src/main/java/cc/unilock/ae2mixin/mixin/client/MEStorageScreenMixin.java
@@ -1,11 +1,13 @@
 package cc.unilock.ae2mixin.mixin.client;
 
 import appeng.client.gui.me.common.MEStorageScreen;
+import appeng.client.gui.me.items.CraftingTermScreen;
+import appeng.client.gui.me.items.PatternEncodingTermScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import yalter.mousetweaks.api.MouseTweaksDisableWheelTweak;
 
 @MouseTweaksDisableWheelTweak
-@Mixin(MEStorageScreen.class)
+@Mixin(value = {CraftingTermScreen.class, MEStorageScreen.class, PatternEncodingTermScreen.class})
 public class MEStorageScreenMixin {
 	// NO-OP
 }
